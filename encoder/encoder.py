4f396ebf99cb0beb95efafe459531a0886ede9b1
pritishjr <pritishjr.personal@gmail.com>
2026-07-24 03:29:24 +0530

    feat: create Encoder

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -5,10 +5,9 @@ import torch.nn.functional as F
 
 class Encoder(nn.Module):
     
-    def __init__(self, conv_strides: int, input_dim: 1, latent_dim: 128 ):
+    def __init__(self, conv_strides: list[int], input_dim: 1, latent_dim: 128 ):
         super().__init__()
         
-        self.conv_strides = conv_strides
-        
+        self.conv_strides = [2,4,5,8] 
         
         
